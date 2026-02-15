dff69354c86bffb03e8efe8d9a1c1d3a326596eb
mouha123 <mouhadiop787@gmail.com>
2026-02-15 17:27:31 +0000

    Accueil

--- ccc.js
+++
@@ -1,8 +0,0 @@
-if (localStorage.theme === 'dark') {
-  html.classList.add('dark')
-}
-
-btn.addEventListener('click', () => {
-  html.classList.toggle('dark')
-  localStorage.theme = html.classList.contains('dark') ? 'dark' : 'light'
-})
--- python.py
+++
@@ -1,3 +0,0 @@
-nom = input("Veuillez saisir votre nom")
-age = int (input ("Saisi ton age"))
-print(f"Bonjour{nom},vous avez {age} ans") 
